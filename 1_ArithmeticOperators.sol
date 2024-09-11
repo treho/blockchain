e3e41744c6f82658864b117b4e20b6d025c69278
treho <33775432+treho@users.noreply.github.com>
2024-09-11 17:01:45 +0200

    Add files via upload

---
+++ 1_ArithmeticOperators.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.22;
+
+contract ArithmeticOperators {
+ 
+    uint16 public a = 10;
+    uint16 public b = 3;
+ 
+    // addition
+    uint public add = a + b;
+ 
+    // subtraction
+    uint public sub = a - b;
+ 
+    // multiplication
+    uint public mul = a * b;
+ 
+    // division
+    uint public div = a / b;
+ 
+    //modulo
+    uint public mod = a % b;
+ 
+    // decrement value
+    // uint public dec = --b;
+ 
+    // increment value
+    // uint public inc = ++a;
+     
+}
