7379eb1e8860540ba24dbca86b2560801f95c882
treho <33775432+treho@users.noreply.github.com>
2025-10-29 16:08:17 +0100

    Add files via upload

---
+++ Calculate.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >0.8.22;
+
+contract Calculate {
+    uint public number1 = 3;
+    uint public number2 = 2;
+
+    uint public addition = number1 + number2;
+}
